fcc13ee90e8a10872c612dcc8f06138b9d82404d
Lucas E. Gitirana <gitiranalucas5@gmail.com>
2025-06-19 14:46:53 -0300

    ajuste de CORS

--- demo/src/main/java/com/pet_love/demo/DemoApplication.java
+++ demo/src/main/java/com/pet_love/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package com.pet_love.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configCORS( ) {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
 	}
 
 }
